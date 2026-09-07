4a017a4ba7b6cc18b17c1e11093626b260c9315f
Kevin Nguyen (Thai Hoang) <hoangnguyen7492@gmail.com>
2026-09-07 22:02:06 +0700

    Fix controller tests to use real routes

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -2,18 +2,17 @@ require 'test_helper'
 
 class CurrenciesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get currencies_index_url
+    get root_url
     assert_response :success
   end
 
-  test "should get search" do
-    get currencies_search_url
+  test "should post search" do
+    post search_url
     assert_response :success
   end
 
-  test "should get calculate" do
-    get currencies_calculate_url
+  test "should post calculate" do
+    post calculate_url
     assert_response :success
   end
-
 end
