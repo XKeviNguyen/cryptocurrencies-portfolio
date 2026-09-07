90287e43457c33ab9f9933cafe0d506849245b37
Kevin Nguyen (Thai Hoang) <hoangnguyen7492@gmail.com>
2026-09-07 23:02:21 +0700

    Test currency endpoints with valid request payloads

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -7,12 +7,24 @@ class CurrenciesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should post search" do
-    post search_url
+    post search_url, params: { search: 'mystring' }
+
     assert_response :success
+    payload = JSON.parse(response.body)
+    assert_equal 2, payload.fetch('currencies').size
   end
 
   test "should post calculate" do
-    post calculate_url
+    currency = currencies(:one)
+
+    Currency.stub :find, currency do
+      currency.stub :current_price, 2.0 do
+        post calculate_url, params: { id: currency.id, amount: 3 }
+      end
+    end
+
     assert_response :success
+    payload = JSON.parse(response.body)
+    assert_equal 6.0, payload.fetch('value')
   end
 end
